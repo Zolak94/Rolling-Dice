e2d9cabf4fffdeafe423d205ac95115d3701a51f
zolak <n.zotek94@gmail.com>
2016-09-01 21:22:32 +0200

    Add array and fix menu

--- dice.c
+++ dice.c
@@ -2,65 +2,62 @@
 #include <stdlib.h>
 #include <time.h>
 int main() {
-  int i,j,n,m,k,z,x1,x2,x3,x4,x5,x6;
-  x1=0;
-  x2=0;
-  x3=0;
-  x4=0;
-  x5=0;
-  x6=0;
+  int i,j,n,m,k,z;
   z=0;
+  int x[5];
+  for ( i = 0; i < 6; i++ ){
+    x[i]=0;
+  }
   int choice;
   time_t t;
+  while ( choice!='4') {
   printf("-------Menu-------\n");
   printf("1) Choose dice\n");
   printf("2) Roll dice, update new table \n");
   printf("3) Roll dice, update old table\n");
   printf("4) Exit\n");
   scanf("%d", &choice);
-  if (choice==4)
-   {
+  switch(choice){
+  case 4:
+    printf("\nYOU SELECTED OPTION 4\n");
+    return 0;
+  case 2:
+    printf("\nYOU SELECTED OPTION 2\n");
+    printf("\nHow many dice? ");
+    scanf("%d", &n);
+    if ( n > 10 ) {
       return 0;
     }
-  if (choice==2)
-   {
-      printf("You chose program 2!\n");
-  printf("How many dice? ");
-  scanf("%d", &n);
-  if ( n > 10 ) {
-    return 0;
+    printf("How many rolls? ");
+    scanf("%d", &m);
+    srand((unsigned) time(&t));
+    for ( i = 0; i < m; i++ ) {
+      for ( j = 0; j < n; j++ ) {
+        z = rand()%6+1;
+        if ( z == 1 ) {
+          x[0]++;
+        }
+        if ( z == 2 ) {
+          x[1]++;
+        }
+        if ( z == 3 ) {
+          x[2]++;
+        }
+        if ( z == 4 ) {
+          x[3]++;
+        }
+        if ( z == 5 ) {
+          x[4]++;
+        }
+        if ( z == 6 ) {
+          x[5]++;}
+        }
+      }
+      for ( i = 0; i < 6; i++ ) {
+      printf ("%d: %d\n", ( i + 1 ), x[i] );
+      }
+      break;
+      }
+    return main();
   }
-  printf("How many rolls? ");
-  scanf("%d", &m);
-  srand((unsigned) time(&t));
-  for ( i = 0; i < m; i++ ) {
-    for ( j = 0; j < n; j++ ) {
-      z = rand()%6+1;
-      if ( z == 1 ) {
-        x1++;
-      }
-      if ( z == 2 ) {
-        x2++;
-      }
-      if ( z == 3 ) {
-        x3++;
-      }
-      if ( z == 4 ) {
-        x4++;
-      }
-      if ( z == 5 ) {
-        x5++;
-      }
-      if ( z == 6 ) {
-        x6++;}
-      }
-    }
-    printf ("1:%d\n",x1);
-    printf ("2:%d\n",x2);
-    printf ("3:%d\n",x3);
-    printf ("4:%d\n",x4);
-    printf ("5:%d\n",x5);
-    printf ("6:%d\n",x6);
-  }
-  return main();
 }
