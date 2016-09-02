f6984754a1c86b81f1e1a36d92ff4bc5f8d77483
zolak <n.zotek94@gmail.com>
2016-09-02 13:34:48 +0200

    add function for rolling dice, update option 1

--- dice.c
+++ dice.c
@@ -1,13 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main() {
-  int i,j,n,m,k,z;
-  z=0;
-  int x[5];
-  for ( i = 0; i < 6; i++ ){
+
+void rolling_dice (int n, int m, int z,int *x,int dice_sides) {
+  time_t t;
+  int i,j;
+  printf("\nHow many dice? ");
+  scanf("%d", &n);
+  if ( n > 10 ) {
+    printf("\nYou entered invalid number of dice!\n\n");
+  }
+  printf("How many rolls? ");
+  scanf("%d", &m);
+  srand((unsigned) time(&t));
+  for ( i = 0; i < m; i++ ){
     x[i]=0;
   }
+  for ( i = 0; i < n; i++ ) {
+    for ( j = 0; j < m; j++ ) {
+      z = rand()%dice_sides+1;
+      if ( z == 1 ) {
+        x[0]++;
+      }
+      if ( z == 2 ) {
+        x[1]++;
+      }
+      if ( z == 3 ) {
+        x[2]++;
+      }
+      if ( z == 4 ) {
+        x[3]++;
+      }
+      if ( z == 5 ) {
+        x[4]++;
+      }
+      if ( z == 6 ) {
+        x[5]++;
+      }
+      if ( z == 7 ) {
+        x[6]++;
+      }
+      if ( z == 8 ) {
+        x[7]++;
+      }
+      if ( z == 9 ) {
+        x[8]++;
+      }
+      if ( z == 10 ) {
+        x[9]++;
+      }
+      if ( z == 11) {
+        x[10]++;
+      }
+      if ( z == 12 ) {
+        x[11]++;
+      }
+      if ( z == 13) {
+        x[12]++;
+      }
+      if ( z == 14 ) {
+        x[13]++;
+      }
+      if ( z == 15 ) {
+        x[14]++;
+      }
+      if ( z == 16 ) {
+        x[15]++;
+      }
+      if ( z == 17 ) {
+        x[16]++;
+      }
+      if ( z == 18 ) {
+        x[17]++;
+      }
+      if ( z == 19 ) {
+        x[18]++;
+      }
+      if ( z == 20 ) {
+        x[19]++;}
+      }
+    }
+    for ( i = 0; i < dice_sides; i++ ) {
+    printf ("%d: %d\n", ( i + 1 ), x[i] );
+    }
+}
+int main() {
+  int dice_sides;
+  int i,j,n,m,k,z;
+  z=0;
+  char option;
+  int x[5];
   int choice;
   int dice_choice;
   time_t t;
@@ -31,124 +113,49 @@ int main() {
     scanf("%d", &dice_choice);
     switch(dice_choice){
       case 1:
-      printf("\nYOU SELECTED 4-SIDED DICE\n");
-      printf("\nHow many dice? ");
-      scanf("%d", &n);
-      if ( n > 10 ) {
-        printf("\nYou entered invalid number of dice!\n\n");
-        return main();
-      }
-      printf("How many rolls? ");
-      scanf("%d", &m);
-      srand((unsigned) time(&t));
-      for ( i = 0; i < 4; i++ ){
-        x[i]=0;
-      }
-      for ( i = 0; i < m; i++ ) {
-        for ( j = 0; j < n; j++ ) {
-          z = rand()%4+1;
-          if ( z == 1 ) {
-            x[0]++;
-          }
-          if ( z == 2 ) {
-            x[1]++;
-          }
-          if ( z == 3 ) {
-            x[2]++;
-          }
-          if ( z == 4 ) {
-            x[3]++;
-          }
-        }
-        for ( i = 0; i < 4; i++ ) {
-        printf ("%d: %d\n", ( i + 1 ), x[i] );
-        }
+        printf("\nYOU SELECTED 4-SIDED DICE\n");
+        dice_sides = 4;
+        break;
+      case 2:
+        printf("\nYOU SELECTED 6-SIDED DICE\n");
+        dice_sides = 6;
+        break;
+      case 3:
+        printf("\nYOU SELECTED 8-SIDED DICE\n");
+        dice_sides = 8;
+        break;
+      case 4:
+        printf("\nYOU SELECTED 8-SIDED DICE\n");
+        dice_sides = 10;
+        break;
+      case 5:
+        printf("\nYOU SELECTED 8-SIDED DICE\n");
+        dice_sides = 12;
+        break;
+      case 6:
+        printf("\nYOU SELECTED 8-SIDED DICE\n");
+        dice_sides = 20;
         break;
       }
+      break;
+  case 2:
+    printf("\nYOU SELECTED OPTION 2\n");
+    for ( i = 0; i < dice_sides; i++ ){
+      x[i]=0;
     }
+    printf("\nYou are rolling %d sided dice\n", dice_sides);
+    rolling_dice(n,m,z,x,dice_sides);
+    break;
+  case 3:
+    printf("\nYOU SELECTED OPTION 3\n");
+    for ( i = 0; i < dice_sides; i++ ){
+      x[i]=0;
+    }
+    rolling_dice(n,m,z,x,dice_sides);
     break;
   case 4:
     printf("\nYOU SELECTED OPTION 4\n");
     return 0;
-  case 2:
-    printf("\nYOU SELECTED OPTION 2\n");
-    printf("\nHow many dice? ");
-    scanf("%d", &n);
-    if ( n > 10 ) {
-      printf("\nYou entered invalid number of dice!\n\n");
-      return main();
-    }
-    printf("How many rolls? ");
-    scanf("%d", &m);
-    srand((unsigned) time(&t));
-    for ( i = 0; i < 6; i++ ){
-      x[i]=0;
-    }
-    for ( i = 0; i < m; i++ ) {
-      for ( j = 0; j < n; j++ ) {
-        z = rand()%6+1;
-        if ( z == 1 ) {
-          x[0]++;
-        }
-        if ( z == 2 ) {
-          x[1]++;
-        }
-        if ( z == 3 ) {
-          x[2]++;
-        }
-        if ( z == 4 ) {
-          x[3]++;
-        }
-        if ( z == 5 ) {
-          x[4]++;
-        }
-        if ( z == 6 ) {
-          x[5]++;}
-        }
       }
-      for ( i = 0; i < 6; i++ ) {
-      printf ("%d: %d\n", ( i + 1 ), x[i] );
-      }
-      break;
-      case 3:
-      printf("\nYOU SELECTED OPTION 3\n");
-      printf("\nHow many dice? ");
-      scanf("%d", &n);
-      if ( n > 10 ) {
-        printf("\nYou entered invalid number of dice!\n\n");
-        return main();
-      }
-      printf("How many rolls? ");
-      scanf("%d", &m);
-      srand((unsigned) time(&t));
-      for ( i = 0; i < m; i++ ) {
-        for ( j = 0; j < n; j++ ) {
-          z = rand()%6+1;
-          if ( z == 1 ) {
-            x[0]++;
-          }
-          if ( z == 2 ) {
-            x[1]++;
-          }
-          if ( z == 3 ) {
-            x[2]++;
-          }
-          if ( z == 4 ) {
-            x[3]++;
-          }
-          if ( z == 5 ) {
-            x[4]++;
-          }
-          if ( z == 6 ) {
-            x[5]++;}
-          }
-        }
-        for ( i = 0; i < 6; i++ ) {
-        printf ("%d: %d\n", ( i + 1 ), x[i] );
-        }
-        break;
-
-      }
-    return main();
   }
 }
