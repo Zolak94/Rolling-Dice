c07cc6695f495c29668c69eb5cbf543ef19c5891
zolak <n.zotek94@gmail.com>
2016-09-03 15:08:55 +0200

    Add function for choosing dice

--- dice.c
+++ dice.c
@@ -3,6 +3,46 @@
 #include <stdlib.h>
 #include <time.h>
 
+int choosing_dice(int *dice_sides) {
+  int dice_choice;
+  printf("\nYOU SELECTED OPTION 1\n");
+  printf("-------Dice-------\n");
+  printf("1) 4-sides\n");
+  printf("2) 6-sides \n");
+  printf("3) 8-sides\n");
+  printf("4) 10-sides\n");
+  printf("5) 12-sides\n");
+  printf("6) 20-sides\n");
+  scanf("%d", &dice_choice);
+  switch (dice_choice) {
+    case 1:
+      printf("\nYOU SELECTED 4-SIDED DICE\n");
+      *dice_sides = 4;
+      break;
+    case 2:
+      printf("\nYOU SELECTED 6-SIDED DICE\n");
+      *dice_sides = 6;
+      break;
+    case 3:
+      printf("\nYOU SELECTED 8-SIDED DICE\n");
+      *dice_sides = 8;
+      break;
+    case 4:
+      printf("\nYOU SELECTED 10-SIDED DICE\n");
+      *dice_sides = 10;
+      break;
+    case 5:
+      printf("\nYOU SELECTED 12-SIDED DICE\n");
+      *dice_sides = 12;
+      break;
+    case 6:
+      printf("\nYOU SELECTED 20-SIDED DICE\n");
+      *dice_sides = 20;
+      break;
+    }
+    return *dice_sides;
+}
+
 void rolling_dice(int number_of_dice, int dice_roll, int random_number, int *x,
   int dice_sides) {
   time_t t;
@@ -27,14 +67,12 @@ void rolling_dice(int number_of_dice, int dice_roll, int random_number, int *x,
 }
 
 int main() {
-  int dice_sides;
-  dice_sides = 6;
-  int i, j, number_of_dice, dice_roll, random_number;
-  random_number = 0;
+  int i, j, number_of_dice, dice_roll;
+  int dice_sides = 6;
+  int random_number = 0;
   char answer;
   int x[5];
   int choice;
-  int dice_choice;
   time_t t;
   while (choice != '4') {
   printf("-------Menu-------\n");
@@ -45,42 +83,8 @@ int main() {
   scanf("%d", &choice);
   switch (choice) {
   case 1:
-    printf("\nYOU SELECTED OPTION 1\n");
-    printf("-------Dice-------\n");
-    printf("1) 4-sides\n");
-    printf("2) 6-sides \n");
-    printf("3) 8-sides\n");
-    printf("4) 10-sides\n");
-    printf("5) 12-sides\n");
-    printf("6) 20-sides\n");
-    scanf("%d", &dice_choice);
-    switch (dice_choice) {
-      case 1:
-        printf("\nYOU SELECTED 4-SIDED DICE\n");
-        dice_sides = 4;
-        break;
-      case 2:
-        printf("\nYOU SELECTED 6-SIDED DICE\n");
-        dice_sides = 6;
-        break;
-      case 3:
-        printf("\nYOU SELECTED 8-SIDED DICE\n");
-        dice_sides = 8;
-        break;
-      case 4:
-        printf("\nYOU SELECTED 8-SIDED DICE\n");
-        dice_sides = 10;
-        break;
-      case 5:
-        printf("\nYOU SELECTED 8-SIDED DICE\n");
-        dice_sides = 12;
-        break;
-      case 6:
-        printf("\nYOU SELECTED 8-SIDED DICE\n");
-        dice_sides = 20;
-        break;
-      }
-      break;
+    choosing_dice(&dice_sides);
+    break;
   case 2:
     printf("\nYOU SELECTED OPTION 2\n");
     for ( i = 0; i < dice_sides; i++ ) {
